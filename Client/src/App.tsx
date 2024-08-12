f4efa6c14449b08d77a157c9f9873f6d4ce3aa57
Bunny <varun.verma2020@vitstudent.ac.in>
2024-08-12 19:58:59 +0530

    fixed the importing erorr of ShadCN

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-blue-600 p-5 rounded">
-        Hello world!
-      </h1>
+      <Button>Hellow ShadCN/UI Button</Button>
     </>
   )
 }
