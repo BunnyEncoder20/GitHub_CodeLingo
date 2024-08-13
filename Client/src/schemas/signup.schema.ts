6976a1dad69194b23f3574d2ed625b1870bd656d
BunnyEncoder20 <varun.verma2020@vitstudent.ac.in>
2024-08-13 14:28:58 +0530

    created signup schema

---
+++ Client/src/schemas/signup.schema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod';
+
+export const signupValidationSchema = z.object({
+    username : z
+        .string()
+        .min(5, {message:"username msut be atleast 5 characters long"})
+        .max(20, {message:"username cannot be longer than 20 characters"}),
+    email : z
+        .string()
+        .email({message:"Invalid email address"}),
+    password : z
+        .string()
+        .min(6, "password must be atleast 6 characters long")
+})
